7b3a3d80b0cd82ad5aad81658cdc1f6b18192588
enolgargon <enolgargon@gmail.com>
2019-08-06 23:31:16 +0200

    Improved queues in Abstract Executor
    Now, the main methods revive the message as param. the abstract class manage the queues
    
    Signed-off-by: enolgargon <enolgargon@gmail.com>

--- logic/AbstractExecutor.py
+++ logic/AbstractExecutor.py
@@ -1,37 +1,64 @@
+import time
 from abc import ABC, abstractmethod
 from queue import Queue
 from threading import Thread
 
+from ipcqueue import posixmq
+
 
 class AbstractExecutor(ABC):
     def __init__(self):
+        self.camera_queue = posixmq.Queue('/camera_control')
         self.segment_queue = Queue()
         self.identify_queue = Queue()
         self.thread1 = None
         self.thread2 = None
         self.thread3 = None
 
+    def _dequeue(self, queue):
+        if queue.qsize() > 0:
+            message = queue.get()
+            if message is None:
+                time.sleep(.2)
+            else:
+                return message
+        else:
+            time.sleep(.2)
+        return None
+
     @abstractmethod
-    def preprocess(self):
+    def preprocess(self, message):
         pass
 
     @abstractmethod
-    def segment(self):
+    def segment(self, message):
         pass
 
     @abstractmethod
-    def identify(self):
+    def identify(self, message):
         pass
 
     def __preprocess_and_segment(self):
-        self.preprocess()
-        self.segment()
+        message = self._dequeue(self.camera_queue)
+        if message is not None:
+            self.preprocess(message)
+
+        message = self._dequeue(self.segment_queue)
+        if message is not None:
+            self.segment(message)
+
+    def _dequeue_identify(self):
+        message = self._dequeue(self.identify_queue)
+        if message is not None:
+            self.identify(message)
 
     def create_threads(self):
         self.thread1 = Thread(target=self.__preprocess_and_segment,
                               name=f"{self.__class__.__name__} preprocess and segmentation thread")
-        self.thread2 = Thread(target=self.identify, name=f"{self.__class__.__name__} identification first thread")
-        self.thread3 = Thread(target=self.identify, name=f"{self.__class__.__name__} identification second thread")
+        self.thread2 = Thread(target=self._dequeue_identify,
+                              name=f"{self.__class__.__name__} identification first thread")
+        self.thread3 = Thread(target=self._dequeue_identify,
+                              name=f"{self.__class__.__name__} identification second thread")
 
         self.thread1.start()
         self.thread2.start()
