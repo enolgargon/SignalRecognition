6ed78294a3881159f9fe552da9d60e674fecec1e
enolgargon <enolgargon@gmail.com>
2019-08-11 22:54:09 +0200

    Basic code for identify images
    Created a basic code for identify method in executor and created Identificator class which allow to try with three nets using three threads easy
    
    Signed-off-by: enolgargon <enolgargon@gmail.com>

--- logic/SignalExecutor.py
+++ logic/SignalExecutor.py
@@ -1,10 +1,24 @@
+import statistics as stats
+from threading import Thread
+
 import cv2
 
 import proyect_util as util
-from . import AbstractExecutor
+from . import AbstractExecutor, Identificator
 
 
 class SignalExecutor(AbstractExecutor):
+    def __init__(self, nets):
+        super().__init__()
+        self.identificators = []
+        if type(nets) == 'array':
+            for net in nets:
+                self.identificators += [Identificator(net)]
+        elif type(nets) == 'string':
+            self.identificators += [Identificator(nets)]
+        else:
+            TypeError('The constructor param must be of type array or string')
+
     def preprocess(self, message):
         color = cv2.cvtColor(message.content, cv2.COLOR_BGR2LAB)
         gauss = cv2.GaussianBlur(color, (9, 9), 0)
@@ -13,13 +27,13 @@ class SignalExecutor(AbstractExecutor):
                                                                      'A frame was read and preprocessed'))
 
     def segment(self, message):
-        (contornos, hierarchy) = cv2.findContours(message.content.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        (contours, hierarchy) = cv2.findContours(message.content.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
         hierarchy = hierarchy[0]
 
         margin = 10
-        for i in range(len(contornos)):
+        for i in range(len(contours)):
             if hierarchy[i][2] >= 0 and hierarchy[i][3] >= 0:
-                c = contornos[i]
+                c = contours[i]
                 x, y, w, h = cv2.boundingRect(c)
                 if abs(w - h) < 10:
                     util.queue_util.put(self.identify_queue,
@@ -27,3 +41,22 @@ class SignalExecutor(AbstractExecutor):
                                                              message.content[x - margin:y - margin,
                                                              x + w + margin:y + h + margin],
                                                              'Possible signal extracted from frame'))
+
+    def identify(self, message):
+        threads = []
+        for identificator in self.identificators:
+            thread = Thread(target=identificator.net, args=(message.content,),
+                            name=f"Identificator of net {identificator.net_name}")
+            threads += [thread]
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        result = []
+        for identificator in self.identificators:
+            result += [identificator.result]
+
+        signal = stats.mode(result)
+        if result.count(signal) > len(result) / 2:
+            print('Success')
