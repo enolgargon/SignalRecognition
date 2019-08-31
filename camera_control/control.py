cd8b4cabac25a8ff031f108499c07eec804a3821
enolgargon <enolgargon@gmail.com>
2019-08-31 19:33:09 +0200

    Logs in Camera Control
    Added logs to camera control module
    
    Signed-off-by: enolgargon <enolgargon@gmail.com>

--- camera_control/control.py
+++ camera_control/control.py
@@ -8,22 +8,30 @@ import proyect_util as util
 
 
 def init():
+    util.LoggerControl().get_logger('control_camera').info('Camera control is initializing...')
+
     cap = cv2.VideoCapture('../../prueba.mp4')
+    util.LoggerControl().get_logger('control_camera').debug('Video capture initialized')
+
     queue = posixmq.Queue('/camera_control')
+    util.LoggerControl().get_logger('control_camera').info('Initialized posixmq queue')
 
     try:
         while cap.isOpened():
             read_flag, frame = cap.read()
             if not read_flag:
-                # log error
+                util.LoggerControl().get_logger('control_camera').error('Error while new frame was reading')
                 pass
             else:
                 util.put(queue, util.Message('control_camera', 'New frame', frame, 'New frame has '
                                                                                    'been read',
                                              datetime.now().strftime('%Y%m%d%H%M%S%f')))
                 time.sleep(.5)
+    except:
+        util.LoggerControl().get_logger('control_camera').error('Un handler error', exc_info=True)
     finally:
         cap.release()
+        util.LoggerControl().get_logger('control_camera').info('Capture release')
 
 
 if __name__ == '__main__':
